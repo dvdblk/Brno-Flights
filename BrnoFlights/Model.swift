fd3ac66d73a494cf8b9e4c5c86be1f1b7bc9aa97
dvdblk <david.bielik1@gmail.com>
2016-02-29 00:04:34 +0100

    2

--- BrnoFlights/Model.swift
+++ BrnoFlights/Model.swift
@@ -55,5 +55,43 @@ class FlightData {
     var flights: [DataCell] = []
 }
 
-class Parser
+class Parser {
+    
+    var json: JSON!
+    
+    func parseJson() -> [DataCell] {
+        var tempDCArr: [DataCell] = []
+        func parse(j: JSON) -> DataCell {
+            var tempDC = DataCell()
+            tempDC.flyTo = j["flyTo"].stringValue
+            tempDC.flyDuration = j["fly_duration"].stringValue
+            tempDC.price = j["price"].intValue
+            tempDC.cityTo = j["cityTo"].stringValue
+            tempDC.flyFrom = j["flyFrom"].stringValue
+            tempDC.dTime = j["dTime"].intValue
+            tempDC.cityFrom = j["cityFrom"].stringValue
+            tempDC.aTime = j["aTime"].intValue
+            let route = j["route"]
+            let transfers = route.count
+            tempDC.transfers = transfers
+            for i in 0..<transfers {
+                tempDC.route.append(parse(route[i]))
+            }
+            return tempDC
+        }
+        
+        for (_, subJson):(String, JSON) in json {
+            tempDCArr.append(parse(subJson))
+        }
+        return tempDCArr
+    }
+    
+    init() {
+        if let path = NSBundle.mainBundle().pathForResource("flights", ofType: "json") {
+            if let jsonData = NSData(contentsOfFile: path) {
+                self.json = JSON(data: jsonData)["data"]
+            }
+        }
+    }
+}
 
